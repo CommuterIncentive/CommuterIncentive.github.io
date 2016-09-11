374f494f2be8c99237fcc2ba9a160a39d335489a
Stephanie Hutson <stephanie.m.hutson@gmail.com>
2016-09-11 07:52:11 -0700

    probs not final commit, but last one while I figure out the presentation

--- employer.js
+++ employer.js
@@ -23,7 +23,7 @@ function addWinners() {
       var person = peeps[i]
       var klass = i === 0 ? 'winner' : 'not'
       var rank = i + 1
-      $('table tr:last').after(
+      $('table.winners tr:last').after(
         '<tr class=' + klass +'><td>'+ rank + '</td><td><img src="'+person.avatar+'"></img></td><td>' + person.name + '</td><td class="points">' + person.points + '</td></tr>'
         )
       if (person.name === 'Stephanie') {
@@ -33,33 +33,55 @@ function addWinners() {
   });
 }
 function delayedAlert() {
-  var timeoutID = window.setTimeout(getWinner, 3000);
+  var timeoutID = window.setTimeout(getWinner, 2000);
 }
 function removeAlert(id) {
-  window.setTimeout(function(){$(id).hide()}, 2000);
+  window.setTimeout(function(){$(id).hide()}, 1000);
 }
 
-// function flash() {
-//     console.log('flashing?')
-//     // $('body').addClass("flash");
-//     $('.winner').addClass('flashy')
-//     window.setTimeout($('.winner').removeClass('flashy'), 500)
-// }
-
-// function bePretty(){
-// }
-
-function updateResults(){
-  // this.hide()
-  $('#startTrip').hide()
+function doThings(row) {
   $.get('https://thingspace.io/dweet/for/SKS7-2e10?latLong=37.7741740554,-122.436788704,12', function(data) {
+  // console.log(row)
+
+    $.get('https://thingspace.io/dweet/for/SKS7-2e10?latLong=37.7741740554,-122.436788704,12', function(data) {
+    })
+    $('#now').html('<h3>' + row.latitude + ', ' + row.longitude + ' at '+ row.timestamp + '</h3>')
+  })
+  // $('table tr:last').after(
+  //   '<tr><td>' + row.timestamp + '</td><td>' + row.latitude + '</td><td>' + row.longitude + '</td><td>' + true + '</td></tr>'
+  //   )
+  if (row.latitude === 37.7741740554) {
     $('.stephanie .points').html(1 + parseInt($('.stephanie .points').html() ))
     $('#first').show()
+  }
+  else if (row.latitude === 37.7770008073) {
+    $('.stephanie .points').html(1 + parseInt($('.stephanie .points').html() ))
+    $('#second').show()
+  }
+}
+
+
+function getResults(){
+  $.get('/data/mockArduino.json', function( data ){
+    for (var i = 0; i < data.data.length; i ++ ){
+      var row = data.data[i]
+      doThings(row)
+    }
   })
+
 
 
   delayedAlert()
   removeAlert('#first')
+
+}
+
+function updateResults(){
+  // this.hide()
+  $('#now').show()
+  $('#startTrip').hide()
+
+  getResults()
 }
 
 function getWinner(){
@@ -82,7 +104,7 @@ function getWinner(){
           var person = peeps[i]
           var klass = i === 0 ? 'winner' : 'not'
           var rank = i + 1
-          $('table tr:last').after(
+          $('table.winners tr:last').after(
             '<tr class=' + klass +'><td>'+ rank + '</td><td><img src="'+person.avatar+'"></img></td><td>' + person.name + '</td><td class="points">' + person.points + '</td></tr>'
             )
         }
