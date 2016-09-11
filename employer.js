1b789f10fcbf51781674018da5ee30defa1ece96
Stephanie Hutson <stephanie.m.hutson@gmail.com>
2016-09-11 00:17:29 -0700

    mock change data in case arduino doesn't work

--- employer.js
+++ employer.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   // console.log("hello world")
   addWinners()
+  $('#refresh').click(updateResults)
 })
 
 
@@ -19,9 +20,33 @@ function addWinners() {
     for (var i= 0; i < peeps.length; i++) {
       var person = peeps[i]
       var klass = i === 0 ? 'winner' : 'not'
+      var rank = i + 1
       $('table tr:last').after(
-        '<tr class=' + klass +'><td>'+ i + '</td><td><img src="'+person.avatar+'"></img></td><td>' + person.name + '</td><td>' + person.points + '</td></tr>'
+        '<tr class=' + klass +'><td>'+ rank + '</td><td><img src="'+person.avatar+'"></img></td><td>' + person.name + '</td><td>' + person.points + '</td></tr>'
         )
     }
   });
+}
+
+function updateResults(){
+  $.getJSON('/data/change_stats.json', function(data){
+    var peeps = data.people.sort(function(a, b){
+      if (a.points < b.points){
+        return 1
+      }
+      if (a.points > b.points){
+        return -1
+      }
+      return 0
+    })
+    $("table").find("tr:gt(0)").remove();
+    for (var i= 0; i < peeps.length; i++) {
+      var person = peeps[i]
+      var klass = i === 0 ? 'winner' : 'not'
+      var rank = i + 1
+      $('table tr:last').after(
+        '<tr class=' + klass +'><td>'+ rank + '</td><td><img src="'+person.avatar+'"></img></td><td>' + person.name + '</td><td>' + person.points + '</td></tr>'
+        )
+    }
+})
 }
